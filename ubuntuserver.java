f6c1081dd60a9938220da978964e02af83ac6730
amitlembhe03 <amitlembhe333@gmail.com>
2024-03-11 03:01:51 +0000

    pushing

--- ubuntuserver.java
+++ ubuntuserver.java
@@ -2,7 +2,7 @@ class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, from ubuntu"); 
+        System.out.println("Hello, from ubuntu server"; 
 
     }
 
