7bff6a271fee74de507e30356f129dcf49d5f115
amitlembhe03 <amitlembhe333@gmail.com>
2024-03-11 03:18:59 +0000

    pushing third time

--- ubuntuserver.java
+++ ubuntuserver.java
@@ -1,9 +1,5 @@
-class HelloWorld {
-
-    public static void main(String[] args) {
-
-        System.out.println("Hello, from ubuntu server 2"; 
-
-    }
-
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
